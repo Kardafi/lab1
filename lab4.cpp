b9a34e2995461eed9a3d611cc45ea4ffe8a16eac
Karol Nieroba <knier@LAPTOP-KAROL>
2025-05-12 14:14:55 +0200

    lab 4 usunac ModelARXKomponent

--- lab4.cpp
+++ lab4.cpp
@@ -19,28 +19,38 @@
 
 int main() {
 
+	//todo POZBYC SIE ModelARXKompoment!!!!!!!!!!!!!!pominac t¹ klase
         // Tworzenie dwóch modeli ARX jako liœci
         ModelARX arx1({ -0.4 }, { 0.6 }, 1, 0);
-        ModelARX arx2({ -0.4 }, { 0.6 }, 1, 0); 
+        ModelARX arx2({ -0.4 }, { 0.6 }, 1, 0);
+        ModelARX arx3({ 0.7 }, { -0.2 }, 1, 0); // dodatkowy model do po³¹czenia szeregowego
 
+        // Opakowanie modeli jako komponenty
         Komponent* komponent1 = new ModelARXKomponent(arx1);
         Komponent* komponent2 = new ModelARXKomponent(arx2);
+        Komponent* komponent3 = new ModelARXKomponent(arx3);
 
-        // Kompozyt szeregowy
+        // Kompozyt równoleg³y: (arx1 || arx2)
         KompozytRownolegly* rowno = new KompozytRownolegly();
         rowno->dodaj(komponent1);
         rowno->dodaj(komponent2);
 
+        // Kompozyt szeregowy: (arx1 || arx2) -> arx3
+        KompozytSzeregowy* szereg = new KompozytSzeregowy();
+        szereg->dodaj(rowno);     // pierwszy etap - po³¹czenie równoleg³e
+        szereg->dodaj(komponent3); // drugi etap - model szeregowy
+
         // Symulacja
         double wejscie = 1.0;
         for (int i = 0; i < 100; ++i) {
-            double wyjscie = rowno->symuluj(wejscie);
+            double wyjscie = szereg->symuluj(wejscie);
             std::cout << "krok " << i << " -> wyjscie: " << wyjscie << std::endl;
-            wejscie = 0.0; // np. impuls jednostkowy tylko na pocz¹tku
+            wejscie = 0.0; // impuls tylko na pocz¹tku
         }
 
-        delete rowno;
+        delete szereg;
         return 0;
+
 }
 
 #endif // MAIN4
